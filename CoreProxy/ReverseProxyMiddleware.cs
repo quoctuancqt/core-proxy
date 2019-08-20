6457c161c15ccc54a075a9466667e60667fcac32
tcao <tuan.cao.ss@synova-solutions.io>
2019-08-20 16:29:20 +0700

    first commit

---
+++ CoreProxy/ReverseProxyMiddleware.cs
@@ -0,0 +1,138 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreProxy
+{
+    public class ReverseProxyMiddleware
+    {
+        private static readonly HttpClient _httpClient = new HttpClient();
+        private readonly RequestDelegate _nextMiddleware;
+
+        public ReverseProxyMiddleware(RequestDelegate nextMiddleware)
+        {
+            _nextMiddleware = nextMiddleware;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            var targetUri = BuildTargetUri(context.Request);
+
+            if (targetUri != null)
+            {
+                var targetRequestMessage = CreateTargetMessage(context, targetUri);
+
+                using (var responseMessage = await _httpClient.SendAsync(targetRequestMessage, HttpCompletionOption.ResponseHeadersRead, context.RequestAborted))
+                {
+                    context.Response.StatusCode = (int)responseMessage.StatusCode;
+                    CopyFromTargetResponseHeaders(context, responseMessage);
+                    await responseMessage.Content.CopyToAsync(context.Response.Body);
+                    //await ProcessResponseContent(context, responseMessage);
+                }
+                return;
+            }
+            await _nextMiddleware(context);
+        }
+
+        private HttpRequestMessage CreateTargetMessage(HttpContext context, Uri targetUri)
+        {
+            var requestMessage = new HttpRequestMessage();
+            CopyFromOriginalRequestContentAndHeaders(context, requestMessage);
+
+            requestMessage.RequestUri = targetUri;
+            requestMessage.Headers.Host = targetUri.Host;
+            requestMessage.Method = GetMethod(context.Request.Method);
+
+            return requestMessage;
+        }
+
+        private void CopyFromOriginalRequestContentAndHeaders(HttpContext context, HttpRequestMessage requestMessage)
+        {
+            var requestMethod = context.Request.Method;
+
+            if (!HttpMethods.IsGet(requestMethod) &&
+              !HttpMethods.IsHead(requestMethod) &&
+              !HttpMethods.IsDelete(requestMethod) &&
+              !HttpMethods.IsTrace(requestMethod))
+            {
+                var streamContent = new StreamContent(context.Request.Body);
+                requestMessage.Content = streamContent;
+            }
+
+            foreach (var header in context.Request.Headers)
+            {
+                requestMessage.Content?.Headers.TryAddWithoutValidation(header.Key, header.Value.ToArray());
+            }
+        }
+
+        private void CopyFromTargetResponseHeaders(HttpContext context, HttpResponseMessage responseMessage)
+        {
+            foreach (var header in responseMessage.Headers)
+            {
+                context.Response.Headers[header.Key] = header.Value.ToArray();
+            }
+
+            foreach (var header in responseMessage.Content.Headers)
+            {
+                context.Response.Headers[header.Key] = header.Value.ToArray();
+            }
+            context.Response.Headers.Remove("transfer-encoding");
+        }
+        private static HttpMethod GetMethod(string method)
+        {
+            if (HttpMethods.IsDelete(method)) return HttpMethod.Delete;
+            if (HttpMethods.IsGet(method)) return HttpMethod.Get;
+            if (HttpMethods.IsHead(method)) return HttpMethod.Head;
+            if (HttpMethods.IsOptions(method)) return HttpMethod.Options;
+            if (HttpMethods.IsPost(method)) return HttpMethod.Post;
+            if (HttpMethods.IsPut(method)) return HttpMethod.Put;
+            if (HttpMethods.IsTrace(method)) return HttpMethod.Trace;
+            return new HttpMethod(method);
+        }
+
+        private Uri BuildTargetUri(HttpRequest request)
+        {
+            Uri targetUri = null;
+            if (request.Path.Value.Contains("/url/"))
+            {
+                var remainingPathIndex = request.Path.Value.IndexOf("/url/") + 5;
+                var remainingPath = request.Path.Value.Substring(remainingPathIndex, (request.Path.Value.Length - remainingPathIndex));
+                targetUri = new Uri(remainingPath);
+            }
+
+            return targetUri;
+        }
+
+        private async Task ProcessResponseContent(HttpContext context, HttpResponseMessage responseMessage)
+        {
+            var content = await responseMessage.Content.ReadAsByteArrayAsync();
+
+            if (IsContentOfType(responseMessage, "application/json"))
+            {
+                var stringContent = Encoding.UTF8.GetString(content);
+                var newContent = stringContent.Replace("https://demotimesheet.atlassian.net", "http://localhost:5000"); ;
+                await context.Response.WriteAsync(newContent, Encoding.UTF8);
+            }
+            else
+            {
+                await context.Response.Body.WriteAsync(content);
+            }
+        }
+
+        private bool IsContentOfType(HttpResponseMessage responseMessage, string type)
+        {
+            var result = false;
+
+            if (responseMessage.Content?.Headers?.ContentType != null)
+            {
+                result = responseMessage.Content.Headers.ContentType.MediaType == type;
+            }
+
+            return result;
+        }
+    }
+}
